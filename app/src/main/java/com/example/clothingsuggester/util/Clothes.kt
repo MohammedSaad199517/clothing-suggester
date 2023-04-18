a4f5299ea6d13121363e09b559ed79d549db8026
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-18 22:19:27 +0300

    create clothes class

---
+++ app/src/main/java/com/example/clothingsuggester/util/Clothes.kt
@@ -0,0 +1,20 @@
+package com.example.clothingsuggester.util
+
+import com.example.clothingsuggester.R
+
+class Clothes {
+    val listOfClothsSummer = listOf<Int>(
+        R.drawable.summer_1,
+        R.drawable.summer_2,
+        R.drawable.summer_3,
+        R.drawable.summer_4,
+    )
+    val listOfClothsWinter = listOf<Int>(
+        R.drawable.winter_1,
+        R.drawable.winter_2,
+        R.drawable.winter_3,
+        R.drawable.winter_4,
+
+        )
+
+}
