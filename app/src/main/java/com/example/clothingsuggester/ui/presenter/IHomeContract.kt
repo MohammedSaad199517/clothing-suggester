3f872559abb77858554ec72928dccc9e1a6bcb94
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-18 22:17:09 +0300

    update IHomeContract

--- app/src/main/java/com/example/clothingsuggester/ui/presenter/IHomeContract.kt
+++ app/src/main/java/com/example/clothingsuggester/ui/presenter/IHomeContract.kt
@@ -1,24 +1,32 @@
 package com.example.clothingsuggester.ui.presenter
 
+import android.content.Context
 import com.example.clothingsuggester.data.requests.WeatherDataParams
 import com.example.clothingsuggester.data.responses.WeatherDataResponse
 import java.io.IOException
+import java.time.LocalDate
 
 interface IHomeContract {
     interface View {
         fun connectWeatherData(weatherData: WeatherDataResponse)
 
         fun showSuggestionCloths(image: Int)
+        fun onFailRequest(error: IOException)
+
 
     }
 
     interface Presenter {
-        fun fetchData( weatherDataParams: WeatherDataParams)
+        fun fetchData(weatherDataParams: WeatherDataParams)
         fun onSuccess(weatherDataResponse: WeatherDataResponse)
 
         fun onFailure(e: IOException)
 
-        fun showSuggestion(temp:Double)
+
+        fun saveClothesAndCurrentDate(
+            weatherData: WeatherDataResponse,
+            context: Context,
+        )
 
     }
 }
