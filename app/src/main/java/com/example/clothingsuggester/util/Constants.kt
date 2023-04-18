08ac9da3cda7042eae0d0727377895966244f8f1
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-04-18 22:18:42 +0300

    add some constant values

--- app/src/main/java/com/example/clothingsuggester/util/Constants.kt
+++ app/src/main/java/com/example/clothingsuggester/util/Constants.kt
@@ -1,10 +1,17 @@
 package com.example.clothingsuggester.util
 
-object Constants {
-    const val apiKey ="e48ea02d4f5c698ea225e4b41f03758a"
+import com.example.clothingsuggester.R
 
-    fun getWeatherUrlIcon(iconName:String): String {
-        return "https://openweathermap.org/img/wn/${iconName}@2x.png"
-    }
-    const val url ="api.openweathermap.org"
+object Constants {
+    const val API_KEY ="e48ea02d4f5c698ea225e4b41f03758a"
+    fun getWeatherUrlIcon(iconName:String) = "https://openweathermap.org/img/wn/${iconName}@2x.png"
+
+    const val URL ="api.openweathermap.org"
+
+    const val SHARED_PREFS_NAME ="clothing_suggester"
+    const val TODAY_CLOTHES ="clothes"
+    const val CURRENT_DATE_KEY = "current_date"
+    const val MID_TEMP ="18"
+
+
 }
